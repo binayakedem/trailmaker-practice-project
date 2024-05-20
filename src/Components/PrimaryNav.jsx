b1787d4ccb81eaf3c87cbc30d067d2f881c7989d
binayakedem <binayakedem@gmail.com>
2024-05-20 16:25:50 +0545

    last commit

--- src/Components/PrimaryNav.jsx
+++ src/Components/PrimaryNav.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import logo from '../assets/logo1.png'
 import { IoReorderThree } from "react-icons/io5";
 import { MdExpandMore } from "react-icons/md";
+import { Link } from 'react-router-dom';
 const PrimaryNav = () => {
   const[toggle, setToggle]=useState(false)
   const[aboutToggle, setAboutToggle]=useState(false)
@@ -20,30 +21,30 @@ const PrimaryNav = () => {
                 </div>
             </div>
             <div className={`flex list-none flex-col md:flex-row  justify-start md:justify-between items-start md:items-center gap-8`}>
-                    <li className={`${toggle?'hidden':''} flex flex-row justify-center items-center cursor-pointer`} ><a href="/">Home</a></li>
+                    <li className={`${toggle?'hidden':''} flex flex-row justify-center items-center cursor-pointer`} ><Link to="/">Home</Link></li>
                     <li className={`${toggle?'hidden':''} relative flex flex-row justify-center items-center cursor-pointer py-2`} onClick={()=>setAboutToggle(!aboutToggle)} >About <MdExpandMore /></li>
                     {
                       aboutToggle?<div className='border rounded absolute mx-16 lg:top-14 bg-white z-50 flex flex-col justify-start items-center py-6 px-10 gap-4' onMouseLeave={()=>setAboutToggle(!aboutToggle)}>
-                       <a href='/about-consultancy'>Company Profile</a>
-                       <a href='/message-from-md'>Message From Md</a>
+                       <Link to='/about-consultancy'>Company Profile</Link>
+                       <Link to='/message-from-md'>Message From Md</Link>
                       </div>:''
                     }
-                    <li className={`${toggle?'hidden':''} flex flex-row justify-center items-center cursor-pointer`}><a href="/country">Country</a> </li>
+                    <li className={`${toggle?'hidden':''} flex flex-row justify-center items-center cursor-pointer`}><Link to="/country">Country</Link> </li>
                     <li className={`${toggle?'hidden':''} flex flex-row justify-center items-center cursor-pointer`} onClick={()=>setCoursesToggle(!coursesToggle)}>Courses <MdExpandMore /></li>
                     {
                       coursesToggle?<div className=' border rounded absolute ml-16 top-60 lg:ml-64 lg:top-14 bg-white z-50 flex flex-col justify-start items-center py-6 px-10 gap-4 cursor-pointer' onMouseLeave={()=>setCoursesToggle(!coursesToggle)}>
-                       <a href='/courses'>GMAT</a>
-                       <a href='/courses'>IELTS</a>
+                       <Link to='/courses'>GMAT</Link>
+                       <Link to='/courses'>IELTS</Link>
                       </div>:''
                     }
-                    <li className={`${toggle?'hidden':''}`}><a href="/gallery">Gallery</a></li>
-                    <li className={`${toggle?'hidden':''} flex flex-row justify-center items-center cursor-pointer`}><a href="/login">Resource</a></li>
-                    <li className={`${toggle?'hidden':''}`}><a href="/blog">Blog</a></li>
-                    <li className={`${toggle?'hidden':''}`}><a href="/contact">Contact</a></li>
+                    <li className={`${toggle?'hidden':''}`}><Link to="/gallery">Gallery</Link></li>
+                    <li className={`${toggle?'hidden':''} flex flex-row justify-center items-center cursor-pointer`}><Link to="/login">Resource</Link></li>
+                    <li className={`${toggle?'hidden':''}`}><Link to="/blog">Blog</Link></li>
+                    <li className={`${toggle?'hidden':''}`}><Link to="/contact">Contact</Link></li>
             </div>
             <div className={`flex flex-col md:flex-row justify-end items-start md:items-center gap-4 ${toggle?'hidden':''}`}>
-            <button className='bg-green-600 px-4 py-3  text-white md:font-semibold hover:bg-blue-600 duration-500 animation-all ease-in-out'><a href='/login'>Login</a></button>
-            <button className='bg-green-600 px-4 py-3  text-white md:font-semibold hover:bg-blue-600 duration-500 animation-all ease-in-out'><a href='/enquiry'>Enquiry</a></button>
+            <button className='bg-green-600 px-4 py-3  text-white md:font-semibold hover:bg-blue-600 duration-500 animation-all ease-in-out'><Link to='/login'>Login</Link></button>
+            <button className='bg-green-600 px-4 py-3  text-white md:font-semibold hover:bg-blue-600 duration-500 animation-all ease-in-out'><Link to='/enquiry'>Enquiry</Link></button>
             </div>
 
         </div>
